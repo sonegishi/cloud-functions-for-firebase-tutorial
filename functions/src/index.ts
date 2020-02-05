7584088694d0012af9656fb57212aced001a9a96
So Negishi <5055ngs@gmail.com>
2020-02-04 21:47:54 -0500

    Modify the helloWorld function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+    console.log('Hello!')
+    response.send("Hello from this Firecast!");
+});
