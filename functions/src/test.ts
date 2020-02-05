9fa0d9cb52fa3eb17e7cf34ec50d89e38bef2ffd
So Negishi <5055ngs@gmail.com>
2020-02-04 23:43:04 -0500

    Add test.ts for trying async/await

---
+++ functions/src/test.ts
@@ -0,0 +1,13 @@
+async function myFunction(): Promise<string> {
+    try {
+        const rank = await getRank()
+        return 'firebase is #' + rank
+    }
+    catch (err) {
+        return 'Error: ' + err
+    }
+}
+
+function getRank() {
+    return Promise.resolve(1)
+}
