8bb50550ae84df6c58b9fbd3e6c5a56386319be2
So Negishi <5055ngs@gmail.com>
2020-02-04 23:04:59 -0500

    Use a promise to get data from Cloud Firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,19 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    console.log('Hello!')
-    response.send("Hello from this Firecast!");
+export const getBostonWeather = functions.https.onRequest((request, response) => {
+    admin.firestore().doc('cities-weather/boston-ma-us').get()
+    .then(snapshot => {
+        const data = snapshot.data()
+        response.send(data)
+    })
+    .catch(error => {
+        // Handle the error
+        console.log(error)
+        response.status(500).send(error)
+    })
 });
